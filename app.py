bd5638f9e72f4cdaba05e45ecc010dbc1e07e04a
aparnaroy <aparnar@udel.edu>
2024-10-04 19:48:11 -0400

    fixed all src paths and added routes for all pages

--- app.py
+++ app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/start-up')
+def startUp():
+    return render_template('start-up.html')
+
+@app.route('/tasks')
+def mainPage():
+    return render_template('main-page.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
--- static/js/auth.js
+++ static/js/auth.js
@@ -1,9 +1,9 @@
 function signIn() {
     // TODO: Add auth sign in code
-    window.location.href = "start-up.html";
+    window.location.href = "start-up";
 }
 
 function signOut() {
     // TODO: Add auth sign out code
-    window.location.href = "index.html";
+    window.location.href = "index";
 }
